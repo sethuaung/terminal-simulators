53b8bff9b7936e34038f3652e1c41c88728c5398
“sethuaung” <sethuaung@outlook.com>
2025-03-27 17:37:27 +0630

    cmd template

---
+++ worker.js
@@ -0,0 +1,42 @@
+const securityHeaders = {
+        "Content-Security-Policy": "upgrade-insecure-requests",
+        "Strict-Transport-Security": "max-age=1000",
+        "X-Xss-Protection": "1; mode=block",
+        "X-Frame-Options": "DENY",
+        "X-Content-Type-Options": "nosniff",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    sanitiseHeaders = {
+        Server: ""
+    },
+    removeHeaders = [
+        "Public-Key-Pins",
+        "X-Powered-By",
+        "X-AspNet-Version"
+    ];
+
+async function addHeaders(req) {
+    const response = await fetch(req),
+        newHeaders = new Headers(response.headers),
+        setHeaders = Object.assign({}, securityHeaders, sanitiseHeaders);
+
+    if (newHeaders.has("Content-Type") && !newHeaders.get("Content-Type").includes("text/html")) {
+        return new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: newHeaders
+        });
+    }
+
+    Object.keys(setHeaders).forEach(name => newHeaders.set(name, setHeaders[name]));
+
+    removeHeaders.forEach(name => newHeaders.delete(name));
+
+    return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: newHeaders
+    });
+}
+
+addEventListener("fetch", event => event.respondWith(addHeaders(event.request)));
